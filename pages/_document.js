b9341a6574c031abb7517a4c55d1bd2cacd8945e
NandanShettyHS <nandanshetty.hs27@gmail.com>
2026-03-08 13:43:57 +0530

    Added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,17 @@ export default function Document() {
     <Html>
       <Head>
 
+        {/* Google Search Console Verification */}
         <meta
           name="google-site-verification"
           content="EpuiP7PVa-uhprtmeeE1OwOZjCIaJXf0iKqVam2y_SE"
         />
+
+        {/* Website Favicon */}
+        <link rel="icon" href="/favicon.png" />
+
+        {/* Apple devices icon */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
 
       </Head>
       <body>
